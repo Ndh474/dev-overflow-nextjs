bca8363c785092b740bb272dde220fa8aa9110f5
ndh <ndh.sunday@gmail.com>
2024-11-02 22:23:49 +0700

    setup Github And Google Auth

---
+++ app/(root)/page.tsx
@@ -0,0 +1,25 @@
+import { auth, signOut } from "@/auth";
+import { Button } from "@/components/ui/button";
+import ROUTES from "@/constants/routes";
+
+const Home = async () => {
+  const session = await auth();
+  console.log(session);
+
+  return (
+    <>
+      <h1 className="h1-bold font-space-grotesk">Hello Nextjs 15!</h1>
+      <form
+        className="px-10 pt-[100px]"
+        action={async () => {
+          "use server";
+          await signOut({ redirectTo: ROUTES.SIGN_IN });
+        }}
+      >
+        <Button type="submit">Log Out</Button>
+      </form>
+    </>
+  );
+};
+
+export default Home;
